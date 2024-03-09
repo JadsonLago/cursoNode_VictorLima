7b0214ee8f4f8e4909eed9d8da0049c942b1828b
JadsonLago <jadsonlago.consultoria@gmail.com>
2024-03-09 12:01:18 -0300

    criando_formulario

--- index.js
+++ index.js
@@ -1,24 +1,36 @@
 const express = require('express');
 const app = express();
-const handlebars = require('express-handlebars')
+const handlebars = require('express-handlebars');
+const bodyParser = require('body-parser')
 const { Sequelize } = require('sequelize');
 
 // Config    
     // Template engine
-        app.engine('handlebars', handlebars({defaultLayout:'main'}))
+        app.engine('handlebars', handlebars.engine({defaultLayout:'main'}))
         app.set('view engine','handlebars')
+    // Body Parser
+        app.use(bodyParser.urlencoded({extended:false}))
+        app.use(bodyParser.json())    
     // Conexão MySQL
-
         const sequelize = new Sequelize('teste', 'root', 'root', {
             host: 'localhost',
             dialect: 'mysql'
         });
 
 // Rotas
-app.get('cad/', function (req, res) {
-    res.send('Seja Rota de cadatro de POSTS')
+app.get('/cad', function (req, res) {
+    res.render('formulario')
 });
 
+app.post('/add', function (req, res) {
+    titulo = req.body.titulo
+    conteudo = req.body.conteudo
+
+    res.send('Formulario recebido!!!'+
+            'O tilulo é: ' +  titulo +
+            'O conteudo é: '+ conteudo)
+
+});
 
 
 
